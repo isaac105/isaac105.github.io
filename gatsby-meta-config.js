ef68c911784f97f286211b746fc7204dbe68b74d
isaac105 <hmkim735@gmail.com>
2022-06-28 20:11:02 +0900

    Initial meta settings

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `isaac105`,
+  description: `경험하고 공부했던 내용을 정리하는 기술블로그입니다.`,
+  author: `isaac`,
+  introduction: `지속적으로 학습하고 공유하는 것과 대화를 통해 시야를 넓히는 것을 지향합니다. 현재 산업기능요원 현역으로 근무중입니다.`,
+  siteUrl: `https://isaac105.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `isaac105`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
     instagram: ``, // Your Instagram account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  icon: `content/assets/favicon.png`, // Add your favicon
+  keywords: [`blog`, `development`, `python`, `backend`, `devops`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'isaac105/isaac105.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
